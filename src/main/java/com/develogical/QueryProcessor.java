32df62516604e07311525d237678799e876e7b94
Kaylin Li <kaylinli@kleeshp>
2022-09-22 15:59:07 -0400

    update largest

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,7 +6,7 @@ public class QueryProcessor {
 
     public String process(String query) {
         if (query.toLowerCase().contains("largest")) {
-            String[] parts = query.split("%20"); 
+            String[] parts = query.split(" "); 
             ArrayList<Integer> nums = new ArrayList<Integer>();
             for (String s : parts) {
                 if (Character.isDigit(s.charAt(0))) 
@@ -18,7 +18,7 @@ public class QueryProcessor {
             return "Sean Connery";
         }
         if (query.toLowerCase().contains("primes")) {
-            String[] parts = query.split("%20"); 
+            String[] parts = query.split(" "); 
             ArrayList<Integer> nums = new ArrayList<Integer>();
             for (String s : parts) {
                 if (Character.isDigit(s.charAt(0))) 
